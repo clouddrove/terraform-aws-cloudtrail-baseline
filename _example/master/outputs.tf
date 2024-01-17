91f49f68918604829f29b67047f642591aed5870
anmolnagpal <anmol@clouddrove.com>
2024-01-17 15:52:25 +0530

    feat: update code and tflint issue

--- _example/master/outputs.tf
+++ _example/master/outputs.tf
@@ -3,11 +3,6 @@ output "cloudtrail_arn" {
   description = "The Amazon Resource Name of the trail"
 }
 
-output "kms_arn" {
-  value       = module.cloudtrail[*].kms_arn
-  description = "The Amazon Resource Name of the kms"
-}
-
 output "tags" {
   value       = module.cloudtrail.tags
   description = "A mapping of tags to assign to the Cloudtrail."
